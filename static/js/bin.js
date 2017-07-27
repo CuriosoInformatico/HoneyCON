cbd717f4a0c79f01cfed2624b04baa511b2eda3c
Samuel López Saura <samuel@mnemo>
2017-07-27 11:28:35 +0200

    Agregada de la funcinalidad de shell y estructura de la aplicación con Vue-JS. Falta dotar de funcionalidad al resto y rellenar el contenido.

---
+++ static/js/bin.js
@@ -0,0 +1,11 @@
+
+let bin  = {
+
+  ls : {
+    options: {'': function(){return 'Listado'}, '-a': function(){return 'folders'}, '-l': null, '-la': null}
+  },
+  cat : {
+    options: {}
+  }
+
+};
